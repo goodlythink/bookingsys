890b3b9d6d6f9e3322e012a7596ccf330bf19aca
GoodlyThink <goodlythink@gmail.com>
2017-09-14 14:22:54 +0700

    create type and update

--- src/actions/departAction.js
+++ src/actions/departAction.js
@@ -1,31 +1,11 @@
 import axios from 'axios';
 import config from '../config';
-
-//Depart List
-export const LOAD_DEPARTS = 'LOAD_DEPARTS';
-export const LOAD_DEPARTS_SUCCESS = 'LOAD_DEPARTS_SUCCESS';
-export const LOAD_DEPARTS_FAILURE = 'LOAD_DEPARTs_FAILURE';
-
-//Depart New, Save, Update
-export const SAVE_DEPART = 'SAVE_DEPART';
-export const SAVE_DEPART_SUCCESS = 'SAVE_DEPARTS_SUCCESS';
-export const SAVE_DEPART_FAILURE = 'SAVE_DEPART_FAILURE';
-
-//Depart Delete
-export const DELETE_DEPART = 'DELETE_DEPART';
-export const DELETE_DEPART_SUCCESS = 'DELETE_DEPART_SUCCESS';
-export const DELETE_DEPART_FAILURE = 'DELETE_FACULTY_FAILURE';
-
-//Get 1 Row
-export const LOAD_DEPART = 'LOAD_DEPART';
-export const LOAD_DEPART_SUCCESS = 'LOAD_DEPART_SUCCESS';
-export const LOAD_DEPART_FAILURE = 'LOAD_DEPART_FAILURE';
-export const RESET_DEPART = 'RESET_DEPART';
+import * as types from './departType';
 
 export const loadDeparts = (term = '') => {
   return (dispatch) => {
     dispatch({
-      type: LOAD_DEPARTS
+      type: types.LOAD_DEPARTS
     })
     axios.get(`${config.ROOT_URL}/departs?term=${term}`, {
       headers: {
@@ -39,10 +19,10 @@ export const loadDeparts = (term = '') => {
       if (response.status !== 200) {
         throw Error(response.statusText);
       }
-      dispatch(departSuccess(LOAD_DEPARTS_SUCCESS, response.data));
+      dispatch(departSuccess(types.LOAD_DEPARTS_SUCCESS, response.data));
     })
       .catch(err => {
-        dispatch(departFailure(LOAD_DEPARTS_FAILURE, err))
+        dispatch(departFailure(types.LOAD_DEPARTS_FAILURE, err))
       })
   }
 }
@@ -51,7 +31,7 @@ export const loadDeparts = (term = '') => {
 export const saveDepart = (values, callback) => {
   return (dispatch) => {
     dispatch({
-      type: SAVE_DEPART
+      type: types.SAVE_DEPART
     }); //Show Loading
     axios.post(`${config.ROOT_URL}/depart`, values, {
       headers: {
@@ -67,12 +47,10 @@ export const saveDepart = (values, callback) => {
       if (response.data.status === 421) {
         throw Error("รหัสซ้ำ!! กรุณาเปลี่ยนรหัสใหม่");
       }
-      dispatch({
-        type: SAVE_DEPART_SUCCESS
-      });
+      dispatch({type: types.SAVE_DEPART_SUCCESS});
       callback();
     }).catch(err => {
-      dispatch(departFailure(SAVE_DEPART_FAILURE, err));
+      dispatch(departFailure(types.SAVE_DEPART_FAILURE, err));
     })
   }
 }
@@ -80,13 +58,13 @@ export const saveDepart = (values, callback) => {
 //======== DELETE =============//
 export const deleteABC = (id, callback)=>{
   return (dispatch)=>{
-    dispatch({type: DELETE_DEPART})
+    dispatch({type: types.DELETE_DEPART})
   }
 }
 export const deleteDepart = (id, callback) => {
   return (dispatch) => {
     dispatch({
-      type: DELETE_DEPART
+      type: types.DELETE_DEPART
     }) //Show Loading
     axios.get(`${config.ROOT_URL}/faculty/delete${id}`, {
       headers: {
@@ -101,11 +79,11 @@ export const deleteDepart = (id, callback) => {
       }
 
       dispatch({
-        type: DELETE_DEPART_SUCCESS
+        type: types.DELETE_DEPART_SUCCESS
       });
       callback()
     }).catch(err => {
-      dispatch(departFailure(DELETE_DEPART_FAILURE, err))
+      dispatch(departFailure(types.DELETE_DEPART_FAILURE, err))
     })
   }
 }
@@ -115,7 +93,7 @@ export const loadDepart = (id, callback) => {
   return (dispatch) => {
     dispatch(resetDepart());
     dispatch({
-      type: LOAD_DEPART
+      type: types.LOAD_DEPART
     });
     axios.get(`${config.ROOT_URL}/depart/${id}`, {
       headers: {
@@ -129,17 +107,17 @@ export const loadDepart = (id, callback) => {
       if (response.data.invalidToken) {
         throw Error(config.ErrorInvalidTokenText);
       }
-      dispatch(departSuccess(LOAD_DEPART_SUCCESS, response.data));
+      dispatch(departSuccess(types.LOAD_DEPART_SUCCESS, response.data));
       callback();
     }).catch(err => {
-      dispatch(departFailure(LOAD_DEPART_FAILURE, err))
+      dispatch(departFailure(types.LOAD_DEPART_FAILURE, err))
     })
   }
 }
 
 export const resetDepart = () => {
   return {
-    type: RESET_DEPART
+    type: types.RESET_DEPART
   }
 }
 
--- src/reducers/departReducer.js
+++ src/reducers/departReducer.js
@@ -1,10 +1,4 @@
-import {
-    LOAD_DEPARTS, LOAD_DEPARTS_SUCCESS, LOAD_DEPARTS_FAILURE,
-    SAVE_DEPART, SAVE_DEPART_SUCCESS, SAVE_DEPART_FAILURE,
-    DELETE_DEPART, DELETE_DEPART_SUCCESS, DELETE_DEPART_FAILURE,
-    LOAD_DEPART, LOAD_DEPART_SUCCESS, LOAD_DEPART_FAILURE,
-    RESET_DEPART
-} from '../actions/departAction';
+import * as types from '../actions/departType';
 
 const initialState = {
     departList: { departs: [], loading: false, error: false },
@@ -17,43 +11,43 @@ export default (state = initialState, action) => {
     let error;
     switch (action.type) {
         //Load Departs
-        case LOAD_DEPARTS:
+        case types.LOAD_DEPARTS:
             return { ...state, departList: { departs: [], loading: true, error: false } }
-        case LOAD_DEPARTS_SUCCESS:
+        case types.LOAD_DEPARTS_SUCCESS:
             return { ...state, departList: { departs: action.payload, loading: false, error: false } }
-        case LOAD_DEPARTS_FAILURE:
+        case types.LOAD_DEPARTS_FAILURE:
             error = action.payload || { message: action.payload.message };
             return { ...state, departList: { departs: [], loading: false, error: error } }
 
         //Save Depart
-        case SAVE_DEPART:
+        case types.SAVE_DEPART:
             return { ...state, departSave: { depart: null, loading: true, error: true } }
-        case SAVE_DEPART_SUCCESS:
+        case types.SAVE_DEPART_SUCCESS:
             return { ...state, departSave: { depart: null, loading: false, error: false } }
-        case SAVE_DEPART_FAILURE:
+        case types.SAVE_DEPART_FAILURE:
             error = action.payload || { message: action.payload.message }
             return { ...state, departSave: { depart: null, loading: false, error: error } }
             
             ///Delete Depart
-        case DELETE_DEPART:
+        case types.DELETE_DEPART:
             return { ...state, departDelete: { loading: true, error: false } }
-        case DELETE_DEPART_SUCCESS:
+        case types.DELETE_DEPART_SUCCESS:
             return { ...state, departDelete: { loading: false, error: false } }
-        case DELETE_DEPART_FAILURE:
+        case types.DELETE_DEPART_FAILURE:
             error = action.payload || { message: action.payload.message }
             return { ...state, departDelete: { loading: false, error: error } }
             
             //Detail Depart
-        case LOAD_DEPART:
+        case types.LOAD_DEPART:
             return { ...state, departShow: { depart: [], loading: true, error: false } }
-        case LOAD_DEPART_SUCCESS:
+        case types.LOAD_DEPART_SUCCESS:
             return { ...state, departShow: { depart: action.payload, loading: false, error: false } }
-        case LOAD_DEPART_FAILURE:
+        case types.LOAD_DEPART_FAILURE:
             error = action.payload || { message: action.payload.message }
             return { ...state, departShow: { depart: [], loading: false, error: error } }
         
             //Reset Value
-        case RESET_DEPART:
+        case types.RESET_DEPART:
             return {
                 ...state,
                 departShow: {
